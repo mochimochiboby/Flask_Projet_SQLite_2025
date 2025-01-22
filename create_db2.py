e7e57afa624c699874432c37ceb0e4ae7e442085
mochimochiboby <lahaye809@gmail.com>
2025-01-22 14:32:32 +0100

    Update create_db2.py

--- create_db2.py
+++ create_db2.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Connexion à la base de données SQLite
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect('database2.db')
 
 # Lecture et exécution du fichier de schéma
 with open('schema2.sql') as f:
