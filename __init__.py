e3a2c0a828689069d3b3d30e85f6f1a17170a512
mochimochiboby <lahaye809@gmail.com>
2025-01-23 09:30:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ def est_authentifie():
 
 # Connexion à la base de données
 def get_db_connection():
-    conn = sqlite3.connect('library.db')
+    conn = sqlite3.connect('bibliotheque.db')
     conn.row_factory = sqlite3.Row
     return conn
 
