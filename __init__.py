49ff6787156064d1947c1f5fb9538e158eaef9c4
mochimochiboby <lahaye809@gmail.com>
2025-01-23 09:33:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, jsonify, request, redirect, url_for, session
-import sqlite3
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
 from werkzeug.utils import secure_filename
+import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
-# Fonction pour vérifier si l'utilisateur est authentifié
+# Fonction pour créer une clé "authentifie" dans la session utilisateur
 def est_authentifie():
     return session.get('authentifie')
 
